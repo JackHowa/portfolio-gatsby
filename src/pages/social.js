10abd319fe8ead4ed208e49e4f4982c760860974
jackhowa <jackhowaa@gmail.com>
2018-05-03 08:07:35 -0500

    Add social media links

---
+++ src/pages/social.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from './Link';
+
+const Social = () => (
+    <div>
+      <tr>
+        <td>
+          <Link
+              linkTitle={'Twitter'} iconTitle={'fa fa-twitter'}
+              linkURL={'https://twitter.com/JackHowa'}/>
+        </td>
+        <td>
+
+          <Link linkTitle={'GitHub'} iconTitle={'fa fa-github'}
+                linkURL={'https://github.com/jackhowa'}/>
+        </td>
+        <td>
+          <Link linkTitle={'LinkedIn'} iconTitle={'fa fa-linkedin'}
+                linkURL={'https://www.linkedin.com/in/jackhowa/'}/>
+        </td>
+        <td>
+
+          <Link linkTitle={'FreeCodeCamp'}
+                iconTitle={'fa fa-free-code-camp'}
+                linkURL={'https://www.freecodecamp.org/jackhowa'}/>
+        </td>
+      </tr>
+    </div>
+);
+
+export default Social;
