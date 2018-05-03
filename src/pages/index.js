6f9fca8eaf6ac722e0cd90ec87b8e687b7201c16
jackhowa <jackhowaa@gmail.com>
2018-05-03 07:46:20 -0500

    Modify the basic intro page with my bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import Link from 'gatsby-link';
 
 const IndexPage = () => (
     <div>
-      <h1>Hi Gatsby</h1>
-      <p>Welcome to your new Gatsby portfolio.</p>
-      <p>Now go build something great.</p>
-      <Link to="/page-2/">Go to page 2</Link>
-      <Link to="/bio/">Click for my bio!</Link>
+      <h1>Come See My Projects</h1>
+      <p>I make tea, podcasts and web and mobile apps.</p>
+      <div>
+        <Link to="/bio/">Click for my bio!</Link>
+      </div>
     </div>
 );
 
